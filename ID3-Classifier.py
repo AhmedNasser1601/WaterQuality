6a23a4807efe30d0fbcb5a63fdcfc9ff363d008d
Ahmed Nasser <ahmed20191701016@cis.asu.edu.eg>
2022-04-22 18:39:02 +0200

    Update ID3-Classifier.py

--- ID3-Classifier.py
+++ ID3-Classifier.py
@@ -12,12 +12,12 @@ from sklearn import metrics
 
 Waterfile = (pd.read_csv("water_potability.csv")).dropna()
 
-X = Waterfile.iloc[0:3275,: ]     #Features
+X = Waterfile.iloc[:, 0:8]     #Features
 Y = Waterfile['Potability']       #Label
 X = ((X - X.mean()) / X.std())    #Normalization
 
 # Split dataset into training set and test set => 80% training and 20% testing
-X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size = 0.20, random_state = 1, shuffle = True)
+X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size = 0.40, random_state = 1, shuffle = True)
 
 # Create Decision Tree classifer object
 clf = DecisionTreeClassifier().fit(X_train, y_train)
