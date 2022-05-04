ca71a62f224d876e101e4346be347a5a27e695c2
YossefEFM <87450165+YossefEFM@users.noreply.github.com>
2022-05-04 07:17:08 +0200

    Updates on naive

---
+++ Naive.py
@@ -0,0 +1,46 @@
+import pandas as pd
+from sklearn.preprocessing import LabelEncoder
+from sklearn.model_selection import  train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.naive_bayes import GaussianNB
+from sklearn.metrics import confusion_matrix,accuracy_score,mean_squared_error
+from  sklearn.model_selection import cross_val_score
+
+#Read Data
+dataset = pd.read_csv("water_potability.csv").fillna(0)
+
+x = dataset.iloc[:,:8].values   
+y = dataset.iloc[:,-1].values
+#print(x)
+#print(y)
+
+#Data Preproccessing
+le = LabelEncoder()
+x[:,0] = le.fit_transform(x[:,0])
+
+#Splitting Data
+X_train, X_test, y_train, y_test=train_test_split(x,y ,test_size =0.33,random_state=100,shuffle=True,stratify=y)
+
+sc = StandardScaler()
+X_train =sc.fit_transform(X_train)
+X_test = sc.transform(X_test)
+#print(X_train)
+#print(X_test)
+
+Classifier = GaussianNB()   
+
+#Train data
+Classifier.fit(X_train,y_train)
+
+#Test data
+y_predict = Classifier.predict(X_test)
+
+#Calculate accuracy
+cm = confusion_matrix(y_predict,y_test)
+ac = accuracy_score(y_predict,y_test) *100
+er = mean_squared_error(y_predict,y_test)
+print("Confusion matrix \n",cm)
+print("Accuracy ---->", ac)
+print("Mean square error ----->",er)
+
+print("\n Cross validation \n",cross_val_score(GaussianNB(),X_train,y_train,cv=5))
