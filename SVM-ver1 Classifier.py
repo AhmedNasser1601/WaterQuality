cff0452108604819a998c83d5f150de7f1f179ef
Ahmed Nasser <ahmed20191701016@cis.asu.edu.eg>
2022-05-25 21:53:38 +0200

    Update SVM-ver1 Classifier.py

--- SVM-ver1 Classifier.py
+++ SVM-ver1 Classifier.py
@@ -27,10 +27,6 @@ scaler = MinMaxScaler(feature_range=(0, 1))
 # learning the statistical parameters for each of the data and transforming
 X = scaler.fit_transform(X)
 
-"""
-water =WaterFile.groupby("Potability", group_keys = True)
-watersampling =  water.apply(lambda X:X.sample(frac = 0.7))
-"""
 impu = SimpleImputer(missing_values=np.nan, strategy="median")
 impu.fit(X)
 X = impu.transform(X)
@@ -50,10 +46,10 @@ prediction = Water.predict(X_test)
 
 # Report
 accuracy = Water.score(X_train, y_train)
-print("train accuracy : ", accuracy * 100, "%")
+print("Train Accuracy : ", accuracy * 100, "%")
 
 accuracy = Water.score(X_test, y_test)
-print("test accuracy  : ", accuracy * 100, "%")
+print("Test Accuracy  : ", accuracy * 100, "%")
 
 print("Mean Square Error : ", metrics.mean_squared_error(np.asarray(y_test), prediction))
 
